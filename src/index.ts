a9925f30707a081310f3065b72397b963a84e88d
bakjonghun <bakjonghun@hikick.kr>
2022-01-07 17:21:04 -0500

    change: ecs to lambda migration

--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,35 @@
 import cors from 'cors';
 import express from 'express';
+import i18n from 'i18n';
+import serverless from 'serverless-http';
 import {
   getRouter,
-  logger,
   LoggerMiddleware,
   registerSentry,
   RESULT,
   Wrapper,
 } from '.';
-import i18n from 'i18n';
 
 export * from './controllers';
 export * from './middlewares';
 export * from './routes';
 export * from './tools';
 
-async function main() {
-  logger.info('System / 시스템을 활성화하고 있습니다.');
-  const app = express();
-  registerSentry(app);
+const app = express();
+registerSentry(app);
 
-  if (process.env.NODE_ENV === 'dev') {
-    const { PrismaStudioMiddleware } = await import('express-prisma-studio');
-    app.use('/prisma', PrismaStudioMiddleware(prisma));
-  }
+app.use(cors());
+app.use(i18n.init);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(LoggerMiddleware());
+app.use('/', getRouter());
+app.all(
+  '*',
+  Wrapper(async () => {
+    throw RESULT.INVALID_API();
+  })
+);
 
-  app.use(cors());
-  app.use(i18n.init);
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: true }));
-  app.use(LoggerMiddleware());
-  app.use('/v1/ride', getRouter());
-  app.all(
-    '*',
-    Wrapper(async () => {
-      throw RESULT.INVALID_API();
-    })
-  );
-
-  app.listen(process.env.WEB_PORT, () => {
-    logger.info('System / 시스템이 준비되었습니다.');
-  });
-}
-
-main();
+const options = { basePath: '/v1/ride' };
+export const handler = serverless(app, options);
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const slsw = require('serverless-webpack');
+
+module.exports = {
+  mode: 'none',
+  target: 'node',
+  entry: slsw.lib.entries,
+  externals: [nodeExternals()],
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: ['./locales/**'],
+    }),
+  ],
+  output: {
+    libraryTarget: 'commonjs',
+    filename: '[name].js',
+    path: path.resolve(__dirname, '.webpack'),
+  },
+  optimization: {
+    concatenateModules: false,
+  },
+};
